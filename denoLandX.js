7482f394a4b5b15c39d69c109cd845cbf9bee028
Robert Schultz <robert@cosmicrealms.com>
2021-11-17 10:58:27 -0500

    wip

--- denoLandX.js
+++ denoLandX.js
@@ -1,4 +1,8 @@
-export {DOMParser} from "https://deno.land/x/deno_dom@v0.1.19-alpha/deno-dom-native.ts";
+// The 'native' version of this module is much faster (takes about 50% of the time) but requires ffi/plugin access which requires env/read/write for every single thing that uses it
+// So I only export the wasm version, since it's good enough in most cases. You can require the native version yourself in any project that needs such blazing speed
+// Also, you must run `await initDOMParser();` before you can use DOMParser
+export {initParser as initDOMParser, DOMParser} from "https://deno.land/x/deno_dom@v0.1.19-alpha/deno-dom-wasm-noinit.ts";
+
 export {parse as xmlParse} from "https://deno.land/x/xml@2.0.1/mod.ts";
 
 // These require seeperate import/export lines due to 'export var from' not being available yet: https://github.com/tc39/proposal-export-default-from/issues
---
+++ util/checkDenoLandXUpdates.js
@@ -0,0 +1,9 @@
+import {xu, fg} from "xu";
+import {fileUtil} from "xutil";
+import {path} from "std";
+
+const dataRaw = await fileUtil.readFile(path.join(xu.dirname(import.meta), "..", "denoLandX.js"));
+const matches = dataRaw.match(/https:\/\/deno.land[^"]+/g).map(v => ([v.split("@")[0], v.split("@")[1].substring(0, v.split("@")[1].indexOf("/"))]));
+xu.log`Open these in chrome to determine latest version:\n\t${matches.map(([u, v]) => `${fg.cyanDim(u)} ${fg.yellow(v)}`).join("\n\t")}`;
+
+xu.log`\nThen update versions in: ${fg.greenDim("/mnt/compendium/DevLab/deno/denoLandX.js")}`;
