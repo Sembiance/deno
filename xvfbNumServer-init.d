ed93e7d2165690b6e1bd75fa03266063982f9f3e
Robert Schultz <robert@cosmicrealms.com>
2022-01-11 17:04:09 -0500

    wip

--- xvfbNumServer-init.d
+++ xvfbNumServer-init.d
@@ -2,17 +2,18 @@
 
 depend() {
     need net
-    use dns netmount
+    after netmount
 }
 
 start() {
     ebegin "Starting xvfbNumServer"
-    /mnt/compendium/DevLab/deno/xvfbNumServer-run.sh
+    start-stop-daemon --start --background --chdir /mnt/compendium/DevLab/deno --make-pidfile --pidfile /var/run/xvfbNumServer.pid \
+    --exec /usr/bin/deno -- run --v8-flags=--max-old-space-size=32768 --import-map /mnt/compendium/DevLab/deno/importMap.json --unstable --allow-net --allow-read xvfbNumServer.js
     eend $?
 }
 
 stop() {
     ebegin "Stopping xvfbNumServer"
-    kill `cat /var/run/xvfbNumServer.pid`
+    start-stop-daemon --stop --quiet --pidfile /var/run/xvfbNumServer.pid
     eend $?
 }
