5f40cfe673fdada1d81d198dfa2bc79ee143d936
Robert Schultz <robert@cosmicrealms.com>
2022-01-12 17:01:22 -0500

    wip

--- xutil/encodeUtil.js
+++ xutil/encodeUtil.js
@@ -3,6 +3,7 @@ import {streams} from "std";
 
 // uses iconv to decode the data with encoding fromEncoding and converts to UTF-8
 // for a list of valid encodings, run: iconv --list
+// Detect encoding of a file visually: https://base64.guru/tools/character-encoding
 export async function decode(data, fromEncoding)
 {
 	const p = Deno.run({cmd : ["iconv", "-f", fromEncoding, "-t", "UTF-8"], clearEnv : true, stdout : "piped", stderr : "piped", stdin : "piped"});
@@ -13,5 +14,6 @@ export async function decode(data, fromEncoding)
 	return new TextDecoder("UTF-8").decode(stdoutResult);
 }
 
+// https://en.wikipedia.org/wiki/Mac_OS_Roman
 const MACOS_ROMAN_EXTENDED = ["Ä", "Å", "Ç", "É", "Ñ", "Ö", "Ü", "á", "à", "â", "ä", "ã", "å", "ç", "é", "è", "ê", "ë", "í", "ì", "î", "ï", "ñ", "ó", "ò", "ô", "ö", "õ", "ú", "ù", "û", "ü", "†", "°", "¢", "£", "§", "•", "¶", "ß", "®", "©", "™", "´", "¨", "≠", "Æ", "Ø", "∞", "±", "≤", "≥", "¥", "µ", "∂", "∑", "∏", "π", "∫", "ª", "º", "Ω", "æ", "ø", "¿", "¡", "¬", "√", "ƒ", "≈", "∆", "«", "»", "…", " ", "À", "Ã", "Õ", "Œ", "œ", "–", "—", "“", "”", "‘", "’", "÷", "◊", "ÿ", "Ÿ", "⁄", "€", "‹", "›", "ﬁ", "ﬂ", "‡", "·", "‚", "„", "‰", "Â", "Ê", "Á", "Ë", "È", "Í", "Î", "Ï", "Ì", "Ó", "Ô", "Ⓐ", "Ò", "Ú", "Û", "Ù", "ı", "ˆ", "˜", "¯", "˘", "˙", "˚", "¸", "˝", "˛", "ˇ"];	// eslint-disable-line max-len
 export {MACOS_ROMAN_EXTENDED};
