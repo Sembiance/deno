859c3d9c9edb239cb8d560f89a75db3a20740649
Robert Schultz <robert@cosmicrealms.com>
2023-01-13 16:23:44 -0500

    Switched to local modified deno-dom due to deno-dom issues

--- denoLandX.js
+++ denoLandX.js
@@ -1,7 +1,4 @@
-// The 'native' version of this module is much faster (takes about 50% of the time) but requires ffi/plugin access which requires env/read/write for every single thing that uses it
-// So I only export the wasm version, since it's good enough in most cases. You can require the native version yourself in any project that needs such blazing speed
-// Also, you must run `await initDOMParser();` before you can use DOMParser
-export {initParser as initDOMParser, DOMParser} from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm-noinit.ts";
+export {DOMParser} from "./denoLandX/deno-dom/deno-dom-native.ts";
 
 export {parse as xmlParse, stringify as xmlStringify} from "https://deno.land/x/xml@2.0.4/mod.ts";
 
--- xvfbNumServer-run.sh
+++ xvfbNumServer-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-daemonize -a -c /mnt/compendium/DevLab/deno -p /var/run/xvfbNumServer.pid /usr/bin/deno run --v8-flags=--max-old-space-size=32768,--expose-gc --import-map /mnt/compendium/DevLab/deno/importMap.json --unstable --allow-net --allow-read --allow-env xvfbNumServer.js
+daemonize -a -c /mnt/compendium/DevLab/deno -p /var/run/xvfbNumServer.pid /usr/bin/deno run --v8-flags=--max-old-space-size=32768,--expose-gc --import-map /mnt/compendium/DevLab/deno/importMap.json --unstable --allow-all xvfbNumServer.js
